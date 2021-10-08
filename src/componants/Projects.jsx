38678ec88202c59266169b856c94378820a85583
Anthony <anthonymcgreal@hotmail.co.uk>
2021-10-08 13:08:18 +0100

    added projects

--- src/componants/Projects.jsx
+++ src/componants/Projects.jsx
@@ -1,11 +1,12 @@
 import { useSpring, animated } from 'react-spring';
 import AbsoluteWrapper from "./AbsoluteWrapper";
+import { nc_news_fe, nc_news_be, waypoint, hangman } from '../projects';
 
 const Projects = () => {
 
     const enterLeft = useSpring({
         from: { opacity: 0, transform: 'translate(-100vw, 0)' },
-        to:{ opacity: 0.9, transform: 'translate(0, 0%)' },
+        to:{ opacity: 0.95, transform: 'translate(0, 0%)' },
         delay: 500,
         config:{ tension: 30,
             friction: 5}
@@ -13,7 +14,7 @@ const Projects = () => {
 
     const enterBottom = useSpring({
         from: { opacity: 0, transform: 'translate(0, 100vh)' },
-        to:{ opacity: 0.9, transform: 'translate(0, 0%)' },
+        to:{ opacity: 0.95, transform: 'translate(0, 0%)' },
         delay: 700,
         config:{ tension: 120,
             friction: 6}
@@ -21,7 +22,7 @@ const Projects = () => {
 
     const enterBottom2 = useSpring({
         from: { opacity: 0, transform: 'translate(0, 100vh)' },
-        to:{ opacity: 0.9, transform: 'translate(0, 0%)' },
+        to:{ opacity: 0.95, transform: 'translate(0, 0%)' },
         delay: 1000,
         config:{ tension: 50,
             friction: 5}
@@ -29,7 +30,7 @@ const Projects = () => {
 
     const enterRight = useSpring({
         from: { opacity: 0, transform: 'translate(100vw, 0)' },
-        to:{ opacity: 0.9, transform: 'translate(0, 0%)' },
+        to:{ opacity: 0.95, transform: 'translate(0, 0%)' },
         delay: 800,
         config:{ tension: 30,
             friction: 5}
@@ -38,10 +39,34 @@ const Projects = () => {
         <AbsoluteWrapper>
             <div className="projectsContainer">
                 <div className="projectsContentContainer">
-               <animated.div style={enterLeft} className="projectBox"></animated.div>
-               <animated.div style={enterBottom} className="projectBox"></animated.div>
-               <animated.div style={enterBottom2} className="projectBox"></animated.div>
-               <animated.div style={enterRight} className="projectBox"></animated.div>
+               <animated.div style={enterLeft} className="projectBox">
+                   <div className="projectContent">
+                   <h1>{nc_news_fe.title}</h1>
+                   <img src={nc_news_fe.thumbnail} alt="nc news frontend homepage"/>
+                   <p>{nc_news_fe.description}</p>
+                   </div>
+               </animated.div>
+               <animated.div style={enterBottom} className="projectBox">
+               <div className="projectContent">
+               <h1>{nc_news_be.title}</h1>
+                   <img src={nc_news_be.thumbnail} alt="nc news backend homepage"/>
+                   <p>{nc_news_be.description}</p>
+               </div>
+               </animated.div>
+               <animated.div style={enterBottom2} className="projectBox">
+               <div className="projectContent">
+               <h1>{waypoint.title}</h1>
+                   <img src={waypoint.thumbnail} alt="waypoint homepage"/>
+                   <p>{waypoint.description}</p>
+               </div>
+               </animated.div>
+               <animated.div style={enterRight} className="projectBox">
+               <div className="projectContent">
+               <h1>{hangman.title}</h1>
+                   <img src={hangman.thumbnail} alt="hangman homepage"/>
+                   <p>{hangman.description}</p>
+               </div>
+               </animated.div>
 
                 </div>
            </div>
---
+++ src/projects.js
@@ -0,0 +1,39 @@
+import nc_news_feThumb from '../src/images/nc_news_feThumb.png';
+import nc_news_beThumb from '../src/images/nc_news_beThumb.png';
+import waypointThumb from '../src/images/waypointThumb.png';
+import hangmanThumb from '../src/images/hangmanThumb.png';
+
+const nc_news_fe = {
+  title: 'NC News Frontend Project',
+  description: 'A small reddit style news website',
+  github: 'https://github.com/AnthonyMcGreal/nc-news-frontend_project',
+  site: 'https://loving-jackson-9ef6b2.netlify.app/',
+  thumbnail: nc_news_feThumb,
+};
+
+const nc_news_be = {
+  title: 'NC News Backend Project',
+  description: 'Backend set-up for NC news Project',
+  github: 'https://github.com/AnthonyMcGreal/Backend-Project-NC-News',
+  site: 'https://news-app-anthony-mcgreal.herokuapp.com/api',
+  thumbnail: nc_news_beThumb,
+};
+
+const waypoint = {
+  title: 'Waypoint',
+  description: 'A social tour app to share walks, pictures and descriptions',
+  github: 'https://github.com/thick-hollins/waypoint-fe ',
+  github2: 'https://github.com/cjpearson85/waypoint-be',
+  video: 'https://www.youtube.com/watch?v=rmPnrpoBBiA',
+  thumbnail: waypointThumb,
+};
+
+const hangman = {
+  title: 'Hangman',
+  description: 'The classic Hangman game',
+  github: 'https://github.com/AnthonyMcGreal/Hangman',
+  site: 'https://determined-wozniak-87616d.netlify.app/',
+  thumbnail: hangmanThumb,
+};
+
+export { nc_news_fe, nc_news_be, waypoint, hangman };
