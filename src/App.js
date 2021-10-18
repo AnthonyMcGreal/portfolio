1d87681d9bf4390ccb949a842521bc2718f8c3ef
Anthony <anthonymcgreal@hotmail.co.uk>
2021-10-18 10:01:46 +0100

    removed unnecessary imports

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
-import Footer from './components/Footer';
 import { useState } from 'react';
 import ViewProject from './components/ViewProject';
 
